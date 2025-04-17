47a9febb8eb4803c2ebc2bea4974c3bfef6cc1c2
maelhemon <mael.hemon05@gmail.com>
2025-04-17 17:08:03 +0200

    js première api faite

---
+++ ville_vf.js
@@ -0,0 +1,18 @@
+function retr_post() {
+    let select=document.getElementById("communes") // permet de récupérer le menu déroulant
+    let code_postal = document.getElementById("code_postal").value;
+    select.innerHTML='<option value="">-- Sélectionnez une commune --</option>';
+    fetch('https://geo.api.gouv.fr/communes?codePostal=' + code_postal) //requete get vers l'api
+        .then((response) => response.json()) // conversion du resultat de la requete en json
+        .then((json) => {  // le resultat se transforme en variable json
+            console.log("Réponse API :", json); // affiche dans la console la réponse de l'api
+           let data=json.map(commune=>`<option value="${commune.nom}">${commune.nom}</option>`).join(" ") // déclare une variable data qui va mapper chaque nom des communes présentes dans le json de l'api  via le .join
+           select.innerHTML+=data // l'afficher dans le menu déroulant
+           let insee=json.map(insee=>insee.code) // récupérer dans un tableau via une fonction flêchée et la méthode .map et .code qui permet de récupérer les valeurs du champs code insee de la réponse de l' API
+            console.log(insee)
+        })
+        .catch((error) => {
+            console.error("Erreur API :", error); // affiche dans la console le fail de l'interrogation de l'api
+        });
+        // récupérer depuis l'api le code INSEE      
+}
