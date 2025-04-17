69f9e9967ca6c966611bd6632317f77e49abbe21
maelhemon <mael.hemon05@gmail.com>
2025-04-17 17:13:30 +0200

    Update ville_vf.js

--- ville_vf.js
+++ ville_vf.js
@@ -1,7 +1,7 @@
 function retr_post() {
     let select=document.getElementById("communes") // permet de récupérer le menu déroulant
-    let code_postal = document.getElementById("code_postal").value;
-    select.innerHTML='<option value="">-- Sélectionnez une commune --</option>';
+    let code_postal = document.getElementById("code_postal").value;//récupère le code postal que l'utilisateur rentre
+    select.innerHTML='<option value="">-- Sélectionnez une commune --</option>'; //vide le menu déroulant à chaque reload de la page
     fetch('https://geo.api.gouv.fr/communes?codePostal=' + code_postal) //requete get vers l'api
         .then((response) => response.json()) // conversion du resultat de la requete en json
         .then((json) => {  // le resultat se transforme en variable json
